7a17486a8f9e0f3c1368e06a7e206849a31959f3
Sara C <smcab.tech@gmail.com>
2025-09-12 17:10:19 -0500

    Removed duplicate resource

--- variables.tf
+++ variables.tf
@@ -30,17 +30,3 @@ variable "environment" {
     network_prefix = "10.0"
   }
 }
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  #vpc_security_group_ids = [aws_security_group.blog.id]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld! I'm Learning Terraform"
-  }
-}
