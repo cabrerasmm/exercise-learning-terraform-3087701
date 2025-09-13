3df6df29d139f0ab437977fd9b74e1ebea7907ab
Sara C <smcab.tech@gmail.com>
2025-09-12 20:11:25 -0500

    Removed ALB

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -16,7 +16,6 @@ data "aws_ami" "app_ami" {
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
 
   name = var.environment.name
   cidr = "${var.environment.network_prefix}.0.0/16"
@@ -31,62 +30,26 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.1"
-
-  name      = "blog"
-  min_size  = 1
-  max_size  = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  security_groups     = [module.blog_sg.security_group_id]
-
-  asg_target_group_arns   = module.blog_alb.target_group_arns
-  
-  image_id      = data.aws_ami.app_ami.id
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-}
 
-module "blog_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+  #vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  name = "blog-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
-
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
-    Environment = "dev"
+    Name = "HelloWorld! I'm Learning Terraform"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "5.3.0"
   name    = "${var.environment.name}-blog"
 
+  #vpc_id  = data.aws_vpc.default.id
   vpc_id  = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
