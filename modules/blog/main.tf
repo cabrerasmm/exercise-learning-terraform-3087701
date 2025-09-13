6c4cb941265d727e856994d3d6a881940033353e
Sara C <smcab.tech@gmail.com>
2025-09-12 20:05:02 -0500

    Fixed alb

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -16,6 +16,7 @@ data "aws_ami" "app_ami" {
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
 
   name = var.environment.name
   cidr = "${var.environment.network_prefix}.0.0/16"
@@ -32,18 +33,24 @@ module "blog_vpc" {
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+  version = ">= 9.0.0"
 
   name      = "blog"
   min_size  = 1
   max_size  = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
   
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  launch_template = {
+    image_id      = data.aws_ami.app_ami.id
+    instance_type = var.instance_type
+  }
+
+
+  autoscaling_group = {
+    target_group_arns = module.blog_alb.target_group_arns
+  }
 }
 
 module "blog_alb" {
