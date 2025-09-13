0d916dba1be06b17617d71912eb36a7c5d066f0d
Sara C <smcab.tech@gmail.com>
2025-09-12 19:44:24 -0500

    Added ALB and Autoscaling

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -30,17 +30,52 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.1"
+
+  name      = "blog"
+  min_size  = 1
+  max_size  = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+}
 
-  #vpc_security_group_ids = [aws_security_group.blog.id]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  name    = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  listeners = {
+    ex-http = {
+      port     = 80
+      protocol = "HTTP"
+    }
+  }
 
   tags = {
-    Name = "HelloWorld! I'm Learning Terraform"
+    Environment = "dev"
   }
 }
 
