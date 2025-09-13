9b12b5117ef97e52609b0b449f3f4e9234b86314
Sara C <smcab.tech@gmail.com>
2025-09-12 19:50:55 -0500

    Fixed main.tf

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -39,7 +39,7 @@ module "autoscaling" {
   max_size  = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  #target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
   
   image_id      = data.aws_ami.app_ami.id
@@ -47,22 +47,33 @@ module "autoscaling" {
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http = {
-      port     = 80
-      protocol = "HTTP"
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
     }
-  }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
