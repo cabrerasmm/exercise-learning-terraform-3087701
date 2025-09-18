e676276a57516b7283bb259180dd031ccb3a21a8
Sara C <smcab.tech@gmail.com>
2025-09-18 10:31:31 -0500

    Removed SGs and VPC

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -14,56 +14,12 @@ data "aws_ami" "app_ami" {
   owners = [var.ami_filter.owner]
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [aws_security_group.blog.id]
-  #vpc_security_group_ids = [module.blog_sg.security_group_id]
-
+  
   tags = {
     Name = "hello-world-${var.environment.name}"
     Environment = var.environment.name
   }
-}
-
-resource "aws_security_group" "blog" {
-  name = "blog"
-  description = "Allow http ang https in. Allow everything out."
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
