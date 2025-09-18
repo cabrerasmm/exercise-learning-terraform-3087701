4038f5e5a8eb0dc890f9fea5cfbb725368fbd154
Sara C <smcab.tech@gmail.com>
2025-09-18 12:31:18 -0500

    Put Back SGs and VPC

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -14,12 +14,47 @@ data "aws_ami" "app_ami" {
   owners = [var.ami_filter.owner]
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = var.environment.name
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  
+
+  #vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
+
   tags = {
     Name = "hello-world-${var.environment.name}"
-    Environment = var.environment.name
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name    = "${var.environment.name}-blog"
+
+  #vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.blog_vpc.vpc_id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
